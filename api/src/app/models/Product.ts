69fabc12ae6871cc4e41b971b6365dc73decc344
Pedro Germano <pedrogermano232@gmail.com>
2022-11-16 17:46:52 -0300

    Product model ok done

--- api/src/app/models/Product.ts
+++ api/src/app/models/Product.ts
@@ -17,16 +17,24 @@ export const Product = model('Product', new Schema({
     type: Number,
     required: true,
   },
-  ingredients: [{
-    name: {
-      type: String,
-      required: true,
-    },
-    icon: {
-      type: String,
-      required: true,
-    }
-  }],
+  ingredients: {
+    required: true,
+    type: [{
+      name: {
+        type: String,
+        required: true,
+      },
+      icon: {
+        type: String,
+        required: true,
+      }
+    }],
+  },
+  category: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: 'Category',
+  }
 }));
 
 
