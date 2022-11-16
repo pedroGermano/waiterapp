0b824fae2829d81d15412d64fec6398fe8dc7bdc
Pedro Germano <pedrogermano232@gmail.com>
2022-11-16 15:09:11 -0300

    Models create

--- api/src/index.ts
+++ api/src/index.ts
@@ -4,8 +4,8 @@ import mongoose from 'mongoose';
 mongoose.connect('mongodb://localhost:27017')
   .then(() => {
     const app = express();
-
     const port = 3001;
+
     app.listen(port, () => {
       console.log(`🚀 Server is running on http://localhost${port}`);
     });
