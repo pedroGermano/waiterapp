150d80e2e2cd34692585da1f4a6915290a9a9bb1
Pedro Germano <pedrogermano232@gmail.com>
2022-11-16 14:46:21 -0300

    Conect mongodb

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,1 +1,13 @@
-console.log('1223234');
+import express  from 'express';
+import mongoose from 'mongoose';
+
+mongoose.connect('mongodb://localhost:27017')
+  .then(() => {
+    const app = express();
+
+    const port = 3001;
+    app.listen(port, () => {
+      console.log(`🚀 Server is running on http://localhost${port}`);
+    });
+  })
+  .catch(() => console.log('Erro ao conectar ao mongodb'));
