7a147daf31ca708f6e2a7c9bcef8df6ce88701a2
Pedro Germano <pedrogermano232@gmail.com>
2022-11-16 21:41:32 -0300

    Routes done

---
+++ api/src/router.ts
@@ -0,0 +1,40 @@
+import { Router } from 'express';
+
+export const router = Router();
+
+router.get('/categories', (req, res) => {
+  res.send('ok');
+});
+
+router.post('/categories', (req, res) => {
+  res.send('ok');
+});
+
+router.get('/products', (req, res) => {
+  res.send('ok');
+});
+
+router.post('/products', (req,  res) => {
+  res.send('ok');
+});
+
+router.get('/categories/:categoryId/products', (req, res) => {
+  res.send('oks');
+});
+
+router.get('/orders', (req, res) => {
+  res.send('ok');
+});
+
+router.post('/orders', (req, res) => {
+  res.send('ok');
+});
+
+router.patch('/orders/:orderId', (req, res) => {
+  res.send('ok');
+});
+
+
+router.delete('/orders/:orderId', (req, res) => {
+  res.send('ok');
+});
