4012aeed78576292d5fede7b328ec7e30ae8bd3b
Pedro Germano <pedrogermano232@gmail.com>
2022-11-20 18:57:30 -0300

    Ok

--- api/src/app/useCases/products/createProduct.ts
+++ api/src/app/useCases/products/createProduct.ts
@@ -4,6 +4,7 @@ import { Product } from '../../models/Product';
 
 export async function createProducts(req: Request, res: Response) {
   try{
+    console.log('...');
 
   } catch (error) {
     console.log(error);
