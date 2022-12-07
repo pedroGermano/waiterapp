5709e5721bf5bc58f7d2b11a89479e02407d0447
Pedro Germano <pedrogermano232@gmail.com>
2022-12-07 17:28:35 -0300

    moongose config done

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,9 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
-const app = express();
+mongoose.set('strictQuery', true);
+mongoose.connect('mongodb://localhost:27017')
+    .then(() => {
+        const app = express();
+        const port = 3001;
+    
+        app.listen(port, () => {
+            console.log(`🚀 Server started at http://localhost:${port}`);
+        });
+    })
 
-const port = 3001;
-
-app.listen(port, () => {
-    console.log(`🚀 Server started at http://localhost:${port}`);
-});
+    .catch(() => console.log('error ao conectar no mongodb'));
