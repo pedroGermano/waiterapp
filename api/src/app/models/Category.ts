94e736df529584599b7f13882760f8162889785f
Pedro Germano <pedrogermano232@gmail.com>
2022-12-07 19:43:00 -0300

    Model done

--- api/src/app/models/Category.ts
+++ api/src/app/models/Category.ts
@@ -1,6 +1,12 @@
 import { model, Schema } from 'mongoose';
 
 export const Category = model('Category', new Schema({
-    name: String,
-    icon: String,
+    name: {
+        type: String,
+        required: true,
+    },
+    icon: {
+        type: String,
+        required: true,
+    },
 }));
