ef57739d9ff052243ec6126d07d7bde1ba32b4c2
Pedro Germano <pedrogermano232@gmail.com>
2022-12-14 20:06:44 -0300

    Api done

--- api/src/router.ts
+++ api/src/router.ts
@@ -9,6 +9,8 @@ import { listProducts } from './app/useCases/products/listProducts';
 import { listProductsByCategory } from './app/useCases/categories/listProductsByCategory';
 import { listOrders } from './app/useCases/orders/listOrders';
 import { createOrder } from './app/useCases/orders/createOrders';
+import { changeOrderStatus } from './app/useCases/orders/changeOrderStatus';
+import { cancelOrder } from './app/useCases/orders/cancelOrder';
 
 export const router = Router();
 
@@ -44,12 +46,8 @@ router.get('/orders', listOrders);
 router.post('/orders', createOrder);
 
 // Change order status
-router.patch('/orders/:ordersId', (req, res) => {
-    res.send('ok');
-});
+router.patch('/orders/:ordersId', changeOrderStatus);
 
 // Delete/cancel 
 
-router.delete('/orders/:orderId', (req, res) => {
-    res.send('ok');
-});
+router.delete('/orders/:orderId', cancelOrder);
