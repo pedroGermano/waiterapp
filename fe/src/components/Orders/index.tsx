ee85750b71eefc477c52b25d272f948ace11046b
Pedro Germano <pedrogermano232@gmail.com>
2022-12-16 18:24:43 -0300

    Board duplicate

--- fe/src/components/Orders/index.tsx
+++ fe/src/components/Orders/index.tsx
@@ -9,6 +9,53 @@ export function Orders() {
           <strong>Fila de espera</strong>
           <span>(1)</span>
         </header>
+
+        <OrdersContainer>
+          <button type='button'>
+            <strong>Mesa 2</strong>
+            <span>2 itens</span>
+          </button>
+          <button type='button'>
+            <strong>Mesa 2</strong>
+            <span>2 itens</span>
+          </button>
+        </OrdersContainer>
+      </Board>
+      <Board>
+        <header>
+          <span>🕑</span>
+          <strong>Fila de espera</strong>
+          <span>(1)</span>
+        </header>
+
+        <OrdersContainer>
+          <button type='button'>
+            <strong>Mesa 2</strong>
+            <span>2 itens</span>
+          </button>
+          <button type='button'>
+            <strong>Mesa 2</strong>
+            <span>2 itens</span>
+          </button>
+        </OrdersContainer>
+      </Board>
+      <Board>
+        <header>
+          <span>🕑</span>
+          <strong>Fila de espera</strong>
+          <span>(1)</span>
+        </header>
+
+        <OrdersContainer>
+          <button type='button'>
+            <strong>Mesa 2</strong>
+            <span>2 itens</span>
+          </button>
+          <button type='button'>
+            <strong>Mesa 2</strong>
+            <span>2 itens</span>
+          </button>
+        </OrdersContainer>
       </Board>
     </Container>
   );
--- fe/src/components/Orders/styles.ts
+++ fe/src/components/Orders/styles.ts
@@ -11,6 +11,10 @@ export const Board = styled.div`
   padding: 16px;
   border: 1px solid rgba(204, 204, 204, 0.4);
   border-radius: 16px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 
   header{
     padding: 8px;
@@ -23,4 +27,38 @@ export const Board = styled.div`
 `;
 
 
-export const OrdersContainer = styled.div``;
+export const OrdersContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    margin-top: 24px;
+
+    button{
+      height: 128px;
+      width: 100%;
+      background: #fff;
+      border: 1px solid rgba(204, 204, 204, 0.4);
+      border-radius: 8px;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      gap: 4px;
+
+      strong {
+        font-weight: 500;
+      }
+
+      span{
+        font-size: 14px;
+        color: #666;
+      }
+
+      & + button{
+        margin-top: 24px;
+      }
+
+    }
+
+
+`;
